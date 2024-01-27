e5e9378323c2ca59c9ea514f06347e338087d4c8
Herukles <121977970+Herukles@users.noreply.github.com>
2024-01-27 21:31:28 +0100

    new import, and LocalDate import delete from aboutMe class
    yes

--- src/main/java/com/Herukles/CVBuilder/CV/ResumeData/AboutMe.java
+++ src/main/java/com/Herukles/CVBuilder/CV/ResumeData/AboutMe.java
@@ -1,9 +1,9 @@
 package com.Herukles.CVBuilder.CV.ResumeData;
 
 import lombok.Data;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 @Data
 @Component
 public class AboutMe {
