45df2173d44fbdd256314ba16a938244aaa05023
Herukles <121977970+Herukles@users.noreply.github.com>
2024-02-04 20:04:11 +0100

    new PersonalInformation class
    Its gonna work with service layer, and because of that layers are more independent from each other.

---
+++ src/main/java/com/Herukles/CVBuilder/CV/Models/PersonalInformation.java
@@ -0,0 +1,28 @@
+package com.Herukles.CVBuilder.CV.Models;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PersonalInformation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String surname;
+    private String countryOfBorn;
+    private LocalDate dateOfBorn;
+    private int age;
+    private String aboutMe;
+}
