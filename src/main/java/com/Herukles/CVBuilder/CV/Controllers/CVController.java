da08ec443953d3a6b6f96d87a85e642429c8b638
Herukles <121977970+Herukles@users.noreply.github.com>
2024-02-07 22:14:48 +0100

    new enpoint created

--- src/main/java/com/Herukles/CVBuilder/CV/Controllers/CVController.java
+++ src/main/java/com/Herukles/CVBuilder/CV/Controllers/CVController.java
@@ -3,7 +3,9 @@ package com.Herukles.CVBuilder.CV.Controllers;
 import com.Herukles.CVBuilder.CV.Models.CV;
 import com.Herukles.CVBuilder.CV.Models.PersonalInformation;
 import com.Herukles.CVBuilder.CV.Services.PersonalInfoService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -25,8 +27,9 @@ public class CVController {
             @PathVariable final long id,
             @RequestBody final PersonalInformation personalInformation) {
             personalInformation.setId(id);
-            final PersonalInformation savedInfo = personalInfoService.create(personalInformation);
+            final PersonalInformation savedPersonalInfo = personalInfoService.create(personalInformation);
+            final ResponseEntity<PersonalInformation> response = new ResponseEntity<PersonalInformation>(savedPersonalInfo, HttpStatus.CREATED);
+            return response;
     }
 
-    //40:43
 }
