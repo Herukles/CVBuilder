d51a8eaf1d4c10ebd4ab21edaf998b6902750c81
Herukles <121977970+Herukles@users.noreply.github.com>
2024-02-08 13:02:08 +0100

    create endpoint with test done

--- src/test/java/com/Herukles/CVBuilder/Controllers/PersonalInfoControllerIT.java
+++ src/test/java/com/Herukles/CVBuilder/Controllers/PersonalInfoControllerIT.java
@@ -6,9 +6,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,6 +20,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
 public class PersonalInfoControllerIT {
+    @Autowired
     private MockMvc mockMvc;
 
     @Test
@@ -27,6 +30,15 @@ public class PersonalInfoControllerIT {
         objectMapper.registerModule(new JavaTimeModule());
         final PersonalInformation personalInformation = TestData.testPersonalInfo();
         final String personalInfoJson = objectMapper.writeValueAsString(personalInformation);
-        mockMvc.perform(MockMvcRequestBuilders.put("/personalInfo/"+personalInformation.getId()).content("application/json")).andExpect(MockMvcResultMatchers.content().json(personalInfoJson));
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/CV/personalInfo/"+personalInformation.getId()).contentType(MediaType.APPLICATION_JSON)
+                .content(personalInfoJson)).
+                andExpect(MockMvcResultMatchers.jsonPath("$.id").value(personalInformation.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(personalInformation.getName()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(personalInformation.getSurname()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.countryOfBorn").value(personalInformation.getCountryOfBorn()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.dateOfBorn").value(personalInformation.getDateOfBorn().toString()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(personalInformation.getAge()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.aboutMe").value(personalInformation.getAboutMe()));
     }
 }
