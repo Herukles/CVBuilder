a4405bce435e08cc368547e39d684c645adc42c8
Herukles <121977970+Herukles@users.noreply.github.com>
2024-02-18 16:42:45 +0100

    delete code not in use.
    Code is replaced with @Builder adnotation in lombok.

--- src/main/java/com/Herukles/CVBuilder/CV/Services/Impl/CVServiceImpl.java
+++ src/main/java/com/Herukles/CVBuilder/CV/Services/Impl/CVServiceImpl.java
@@ -6,6 +6,7 @@ import com.Herukles.CVBuilder.CV.Models.Entities.PersonalInfoEntity;
 import com.Herukles.CVBuilder.CV.Repository.CVRepository;
 import com.Herukles.CVBuilder.CV.Services.CVService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Service
 public class CVServiceImpl implements CVService {
     private final CVRepository cvRepository;
 
